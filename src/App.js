367a59edba7026de561ea10162409b0f2fd493e3
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-11-10 23:03:32 -0800

    the button position changes on mouseover when an invalid email or phone number is entered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+
+    if (id === "emailInput") {
+      setEmail(value);
+    } else if (id === "numberInput") {
+      setNumber(value);
+    }
+
+    // Check validation on input change
+    validateInputs();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check validation on form submit
+    validateInputs();
+  };
+
+  const validateInputs = () => {
+    const isValidEmail = email.includes("@");
+    const isValidPhoneNumber = /^\d+$/.test(number);
+
+    setIsInvalid(!isValidEmail || !isValidPhoneNumber);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="first name"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="last name"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          id="emailInput"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          placeholder="number"
+          id="numberInput"
+          value={number}
+          onChange={handleInputChange}
+        />
+        <button
+          type="submit"
+          id="submitButton"
+          className={isInvalid ? "invalid" : ""}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
