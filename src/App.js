befe1bd9d839feaa9b4857f9632b9644c0af37f9
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-11-16 21:43:10 -0800

    validation with EyeOutlined

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 import "./App.css";
 
 function App() {
   const [email, setEmail] = useState("");
   const [number, setNumber] = useState("");
   const [isMoved, setIsMoved] = useState(false);
+  const [visible, setVisible] = useState(false);
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value)
-  }
+    setEmail(e.target.value);
+  };
 
   const handleNumberChange = (e) => {
-    setNumber(e.target.value)
-  }
+    setNumber(e.target.value);
+  };
+
   const handleMouseOver = () => {
     if (!isValidEmail(email) || !isValidPhoneNumber(number)) {
       if (!isMoved) {
@@ -21,40 +24,26 @@ function App() {
         setIsMoved(false);
       }
     }
-  }
+  };
 
-  //color change 
-  const handleColorChange = () => {
-    if (isValidEmail(email) && isValidPhoneNumber(number)) {
-      return 'green'
-    }else{
-      return 'red';
-    }
-  }
-
-// Check validation on form submit
+  // Check validation on form submit
   const isValidEmail = (email) => {
     return email.includes("@");
-  }
+  };
 
-  const isValidPhoneNumber = (email) => {
+  const isValidPhoneNumber = (number) => {
     return /^\d+$/.test(number);
   };
 
   return (
     <div className="App">
       <form>
-        <input
-          type="text"
-          placeholder="first name"
-        />
+        <ul>
+        <input type="text" placeholder="first name" />
         <br />
 
-        <input
-          type="text"
-          placeholder="last name"
-        />
-        <br /> 
+        <input type="text" placeholder="last name" />
+        <br />
 
         <input
           type="text"
@@ -62,16 +51,40 @@ function App() {
           id="emailInput"
           value={email}
           onChange={handleEmailChange}
+          style={{
+            border: `2px solid
+          ${isValidEmail(email) || email === "" ? "green" : "red"}`,
+          }}
         />
         <br />
-
-        <input
-          type="text"
-          placeholder="number"
-          id="numberInput"
-          value={number}
-          onChange={handleNumberChange}
-        />
+        {/* <label htmlFor="password">pass</label> */}
+        <div className="password-container">
+          <input
+            placeholder="password"
+            type={visible ? "text" : "password"}
+            id="password"
+            value={number}
+            onChange={handleNumberChange}
+            style={{
+              border: `2px solid ${
+                isValidPhoneNumber(number) || number === "" ? "green" : "red"
+              }`,
+            }}
+          />
+          <span
+            className="visibleIcon"
+            onClick={() => setVisible(!visible)}
+            style={{
+              position: "absolute",
+              top: "60%",
+              right: "5px", // Adjust as needed
+              transform: "translateY(-50%)",
+              cursor: "pointer",
+            }}
+          >
+            {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+          </span>
+        </div>
         <br />
 
         <button
@@ -79,12 +92,12 @@ function App() {
           id="submitButton"
           onMouseOver={handleMouseOver}
           style={{
-            transform: isMoved ? `translateX(${100}px)` : 'translateX(0)',
-            backgroundColor: handleColorChange(),
+            transform: isMoved ? `translateX(${100}px)` : "translateX(0)",
           }}
         >
           Submit
         </button>
+        </ul>
       </form>
     </div>
   );
